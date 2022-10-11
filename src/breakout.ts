17b681343a7adfe8488151ae7933ddc3acaa4519
Fabien Lavocat <fabien.lavocat@dolby.com>
2022-10-11 16:24:01 -0700

    Fix issue when leaving a conference

--- src/breakout.ts
+++ src/breakout.ts
@@ -55,6 +55,8 @@ const createBreakout: CreateBreakout = (commands: CommandsType) => {
     };
 
     const onParticipantLeft = async (event: ParticipantLeftNotification) => {
+        if (event.participant.id === VoxeetSDK.session.participant.id) return;
+        
         // Remove the participant from any breakout room
         await moveTo(null, [event.participant]);
     };
